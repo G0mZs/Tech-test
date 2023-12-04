70f294ca80fb1faed093b44e7622f46c4be3964a
Bernardo Gomes <47523445+G0mZs@users.noreply.github.com>
2023-12-04 19:15:52 +0000

    Create Initial Repository

---
+++ CallDetailRecordAPI/CallDetailRecordAPI/Controllers/CdrController.cs
@@ -0,0 +1,20 @@
+﻿using CallDetailRecordAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CallDetailRecordAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CdrController : ControllerBase
+    {
+        /// <summary>The call detail record service.</summary>
+        private readonly ICdrService _cdrService;
+
+        /// <param name="cdrService">The call detail record service.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="cdrService"/></exception>
+        public CdrController(ICdrService cdrService)
+        {
+            _cdrService = cdrService ?? throw new ArgumentNullException(nameof(cdrService));
+        }
+    }
+}
---
+++ CallDetailRecordAPI/CallDetailRecordAPI/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CallDetailRecordAPI.Services;
+using CallDetailRecordAPI.Services.Interfaces;
+
+namespace CallDetailRecordAPI.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICdrService, CdrService>();
+
+            return services;
+        }
+    }
+}
