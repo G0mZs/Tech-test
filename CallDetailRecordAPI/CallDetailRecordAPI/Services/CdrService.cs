b29247d82910ed73099f79fc55696e67f0a514c4
Bernardo Gomes <47523445+G0mZs@users.noreply.github.com>
2023-12-05 12:32:06 +0000

    Create MongoDb Database (#3)
    * Create MongoDB Configuration
    
    * Untrack files in .gitignore

--- CallDetailRecordAPI/CallDetailRecordAPI/Services/CdrService.cs
+++ CallDetailRecordAPI/CallDetailRecordAPI/Services/CdrService.cs
@@ -1,8 +1,21 @@
-﻿using CallDetailRecordAPI.Services.Interfaces;
+﻿using CallDetailRecordAPI.Data.Configurations;
+using CallDetailRecordAPI.Services.Interfaces;
+using CallDetailRecordAPI.Structure.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 
 namespace CallDetailRecordAPI.Services
 {
     public class CdrService : ICdrService
     {
+        private readonly IMongoCollection<CallDetailRecord> _cdrCollection;
+
+        public CdrService(
+            IMongoDatabase database,
+            IOptions<CdrDatabaseConfiguration> options)
+        {
+            _cdrCollection = database.GetCollection<CallDetailRecord>(
+                options.Value.CallRecordsCollectionName);
+        }
     }
 }
---
+++ CallDetailRecordAPI/CallDetailRecordAPI/Structure/Enums/CallType.cs
@@ -0,0 +1,12 @@
+﻿namespace CallDetailRecordAPI.Structure
+{
+    /// <summary>Represents a call type.</summary>
+    public enum CallType
+    {
+        /// <summary>Domestic call type.</summary>
+        Domestic = 1,
+
+        /// <summary>International call type.</summary>
+        International = 2
+    }
+}
